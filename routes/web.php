6f94e7355050d1456f9e2e8e5aa9d1a75b177730
Nguyen Hoang Quan <nguyenhoangquanfc23@gmail.com>
2024-05-11 21:54:26 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
+
+Route::get('/',[MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movies}',[MoviesController::class, 'show'])->name('movies.show');
